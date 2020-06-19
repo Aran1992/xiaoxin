ba7bca6886c4ed5a5a5f7164073ac785ab4e614b
aran1992 <2572261667@qq.com>
2020-06-19 22:58:18 +0800

    还原配置

--- run-option.js
+++ run-option.js
@@ -14,19 +14,19 @@ const RunOption = {
     // 镜头跟随敌方自行车（1就跟随 为0就不跟随）
     cameraFollowEnemy: 0,
     // 是否解除所有系统的限制（1就解除 为0就限制）（测试用，设置这个之后类似的限制都不会有了）
-    unlockAllSystem: 1,
+    unlockAllSystem: 0,
     // 是否显示自行车碰撞体（1就显示 为0就不显示）
     showCollider: 0,
     // 是否启用单机模式（该模式下不需要连接服务器，每次游玩都相当于是从头开始）（1就启用 为0就不启用）
-    singlePlayerMode: 1,
+    singlePlayerMode: 0,
     // 是否开启所有关卡
-    openAllLevel: 1,
+    openAllLevel: 0,
     // 强制显示新手引导 0表现不进行控制，1表示每次都会显示，2表现每次都不会显示
-    forceShowBeginnerGuide: 1,
+    forceShowBeginnerGuide: 0,
     // 强制显示界面引导 0表现不进行控制，1表示每次都会显示，2表现每次都不会显示
-    forceShowUIGuide: 2,
+    forceShowUIGuide: 0,
     // 强制显示的引导列表（可以多个测试用）
-    forceShowUIGuideList: ["无尽模式界面引导", "排名模式界面引导"],
+    forceShowUIGuideList: [],
     // 填写要测试的解锁系统引导，一旦填写了，那么进到GameLevelScene的时候就会马上开始这个引导，还要记得吧unlockAllSystem也打开
     testUnlockSystemGuide: "",
 };
